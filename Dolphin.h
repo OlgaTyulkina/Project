a3a0cf75a61c343c071cf1c38f2cc9fd69e2fc74
OlgaTyulkina <61185779+OlgaTyulkina@users.noreply.github.com>
2020-03-30 14:56:05 +0300

    Create Dolphin.h

---
+++ Dolphin.h
@@ -0,0 +1,14 @@
+#ifndef _Dolphin_h_
+#define _Dolphin_h_
+#include "Mlecopitaushee.h" 
+
+class TDolphin: public TMlecopitaushee
+{
+public:
+	TDolphin();
+	const char* Golos();
+	int Echo();
+	double Moloco();
+};
+
+#endif
