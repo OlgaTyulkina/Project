4b204d397376e0f40f3d6e8bf8dfd4822b602305
OlgaTyulkina <61185779+OlgaTyulkina@users.noreply.github.com>
2020-03-30 14:58:23 +0300

    Create Crocodile.h

---
+++ Crocodile.h
@@ -0,0 +1,16 @@
+#ifndef _Crocodile_h_
+#define _Crocodile_h_
+#include "Reptilia.h"
+
+class TCrocodile : public TReptilia
+{
+public:
+    TCrocodile();
+    const char* Golos();
+    int teeth();
+        //double timeperformance();
+};
+
+
+#endif 
+
