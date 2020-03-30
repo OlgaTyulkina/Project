c513ffea68c4d649a05aa91ef6c0abbed929921a
OlgaTyulkina <61185779+OlgaTyulkina@users.noreply.github.com>
2020-03-30 14:54:21 +0300

    Create Cows.cpp

---
+++ Cows.cpp
@@ -0,0 +1,22 @@
+#include "Cows.h" 
+#include <stdlib.h>
+
+TCow::TCow() : TMlecopitaushee()
+{
+	cvet = 2;
+	kolvokonechnostei = 4;
+	kolvosystems = 5;
+}
+const char* TCow::Golos()
+{
+	return "Muuu";
+}
+
+const char* TCow::Eating()
+{
+	return "grass";
+}
+double TCow::Moloco()
+{
+	return 0.0;
+}
